a41f5cfaed31471373e1335c7ac4df147c303e41
bimashazaman <bimashazaman1234@gmail.com>
2023-12-18 20:52:54 +0600

    Fake data

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ import postRoutes from './routes/posts.js'
 import { register } from './controllers/auth.js'
 import { createPost } from './controllers/posts.js'
 import { verifyToken } from './middleware/auth.js'
+import User from './models/User.js'
+import Post from './models/Post.js'
+import { users, posts } from './data/index.js'
 
 // CONFIGURATIONS
 
@@ -106,5 +109,7 @@ mongoose
   .then(() => {
     // Starting the server on successful database connection
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    // User.insertMany(users)
+    // Post.insertMany(posts)
   })
   .catch((error) => console.log(`${error} did not connect`))
