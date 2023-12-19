12097af02c07c9997e39a5ca6e8b60b08a54c390
bimashazaman <bimashazaman1234@gmail.com>
2023-12-19 20:59:16 +0600

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import HomePage from './scenes/homePage'
 import LoginPage from './scenes/loginPage'
 import ProfilePage from './scenes/profilePage'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from './theme'
 
 function App() {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  // const isAuth = Boolean(useSelector((state) => state.token))
+
   return (
-    <Router>
-      <div className='app'>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/profile/:userId' element={<ProfilePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className='app'>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path='/' element={<LoginPage />} />
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/profile/:userId' element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Navbar from '../navbar/index'
 
 const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
 }
 
 export default HomePage
