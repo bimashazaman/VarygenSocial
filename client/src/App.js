2de5be8bf30069a958d145fb01d76d8c99445f09
bimashazaman <bimashazaman1234@gmail.com>
2023-12-19 19:44:20 +0600

    Route base setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './scenes/homePage'
+import LoginPage from './scenes/loginPage'
+
 function App() {
-  return <div className='app'></div>
+  return (
+    <Router>
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
